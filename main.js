ea845c7db985c41750df6dea342df552a88837ad
troshha <troshha@gmail.com>
2020-02-19 22:07:51 +0200

    fix

--- main.js
+++ main.js
@@ -4,11 +4,29 @@ window.onscroll = function() {
 
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    console.log(123);
     document.getElementById('header').style.backgroundImage =
       'linear-gradient(76deg, #0099cc 0%, #66cc99 90%, #66cc99 100%)';
   } else {
-    console.log(321);
     document.getElementById('header').style.backgroundImage = '';
+  }
+}
+
+function showpopup(e) {
+  e.preventDefault();
+  const form = document.querySelector('#contactForm');
+  form.addEventListener('submit', () => {
+    document.querySelector('.popup').style.display = 'block';
+  });
+  setTimeout(() => {
+    document.querySelector('.popup').style.display = 'none';
+  }, 5000);
+}
+
+function mobilemenu() {
+  var x = document.getElementById('myLinks');
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
+  } else {
+    x.style.display = 'block';
   }
 }
